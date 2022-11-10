c723a9dea8dba3242c642c19624cb186b0397745
MyName <jgabaextra@gmail.com>
2022-11-10 19:09:40 +0100

    now2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
+export const useData = (path) => {
+  
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(path)
+      .then(response => response.json())
+      .then(data => {
+        const noNull = data._embedded.players.filter(function (val) { return val.userName !== '' })
+        console.log(noNull);
+        setData(noNull);
+        setLoading(false);
+      })
+  }, [path, data]);
+
+  return [data, loading];
+};
+
+export const addPerson = async (userName) => {
+  document.getElementById("text-message").value="";
+  await fetch('/players', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ userName })
+  });
+  return fetch('/players');
+};
 
 function App() {
+
+  const [players, loading] = useData('/players');
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+
+
+  const names = players.map(player => <tr><td>{player.userName}</td></tr>);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1 className='display-1 border-bottom border-5 '>Player Roster</h1>
+      <div>
+        <div className='w-50 p-3 d-inline-flex rounded-pill border bg-secondary'>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text bg-light" id="addon-wrapping">New player:</span>
+            <input type="text" className="form-control" placeholder="username" aria-label="Username" aria-describedby="addon-wrapping" id="text-message" />
+            <button type="button" className="btn btn-primary" onClick={() => addPerson(document.getElementById("text-message").value)}> Add</button>
+          </div>
+        </div>
+      </div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Names:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {names}
+        </tbody>
+      </table>
+
+
     </div>
   );
 }
